ea380ad2cf1bafa06f32d62d9e7c4536abe9b49c
Shomi Goland <shlomi@agpulse.com>
2020-04-12 19:26:43 +0300

    Moved to annotation declarations

---
+++ core/src/main/java/academy/learnprogramming/AppConfig.java
@@ -0,0 +1,9 @@
+package academy.learnprogramming;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "academy.learnprogramming")
+public class AppConfig {
+}
--- core/src/main/java/academy/learnprogramming/Main.java
+++ core/src/main/java/academy/learnprogramming/Main.java
@@ -3,23 +3,22 @@ package academy.learnprogramming;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
-    private static final String CONFIG_LOCATION = "beans.xml";
-
     public static void main(String[] args) {
         log.info("Guess The Number Game");
 
         // create context (container)
         ConfigurableApplicationContext context
-                = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+                = new AnnotationConfigApplicationContext(AppConfig.class);
 
         NumberGenerator numberGenerator
-                = context.getBean("numberGenerator", NumberGenerator.class);
+                = context.getBean(NumberGenerator.class);
 
         // call method next() to get a random number
         int number = numberGenerator.next();
